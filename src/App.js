c137f8013bcb20b3b47a865032e04415a6521bb5
Adnan Saleem <adnan13893@gmail.com>
2021-07-09 17:08:43 +0500

    layout additions and changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React, {useState, useEffect} from "react";
+import {experienceURL} from "./utils/constants";
 import "./App.css";
+import ExperienceSection from "./components/experience-section";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      onFetchData();
+    }, 1000);
+  }, []);
+
+  const onFetchData = async () => {
+    let response = await fetch(experienceURL);
+    let result = await response.json();
+    setData(result);
+    console.log(result);
+  };
+
   return (
     <div className="App">
-      <h1>Experience</h1>
+      {data.length ? <h1>Experience</h1> : <h1>Loading...</h1>}
+      <ExperienceSection data={data} />
     </div>
   );
 }
---
+++ src/components/experience-section.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SideNavbar from "./side-navbar";
+import ExperienceDetails from "./experience-details";
+
+const ExperienceSection = ({data}) => {
+  return (
+    <>
+      <SideNavbar data={data} />
+    </>
+  );
+};
+
+export default ExperienceSection;
